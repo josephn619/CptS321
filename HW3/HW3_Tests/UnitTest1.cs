014d33fda19bcf8bd0aafdff11a380ee78b16984
(Adam) Joseph Nassar <45108299+josephn619@users.noreply.github.com>
2022-02-06 02:03:49 -0800

    Added Project

---
+++ HW3/HW3_Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+// <copyright file="UnitTest1.cs" company="Adam Nassar 1158872">
+// Copyright (c) Adam Nassar 1158872. All rights reserved.
+// </copyright>
+
+namespace HW3_Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        /// <summary>
+        /// Sets up nothing.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
