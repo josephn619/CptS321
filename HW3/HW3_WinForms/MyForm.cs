105e52ed1ae3be30e6ab37556fc837cb6cb6c0b7
(Adam) Joseph Nassar <45108299+josephn619@users.noreply.github.com>
2022-02-06 02:02:08 -0800

    Added Project

---
+++ HW3/HW3_WinForms/MyForm.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="MyForm.cs" company="Adam Nassar 11588762">
+// Copyright (c) Adam Nassar 11588762. All rights reserved.
+// </copyright>
+
+namespace HW3_WinForms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Class for WinForms Interface.
+    /// </summary>
+    public partial class MyForm : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyForm"/> class.
+        /// </summary>
+        public MyForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MyForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HW3/HW3_WinForms/Program.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="Program.cs" company="Adam Nassar 11588762">
+// Copyright (c) Adam Nassar 11588762. All rights reserved.
+// </copyright>
+
+namespace HW3_WinForms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Runs application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyForm());
+        }
+    }
+}
