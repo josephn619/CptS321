36d6fad43b642279db02f1953262488ddebc5e38
(Adam) Joseph Nassar <45108299+josephn619@users.noreply.github.com>
2022-02-22 21:45:31 -0800

    Project Added

---
+++ CptS-321_Spreadsheet_Application/Spreadsheet_Adam_Nassar/Program.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="Program.cs" company="Adam Nassar 11588762">
+// Copyright (c) Adam Nassar 11588762. All rights reserved.
+// </copyright>
+
+namespace Spreadsheet_Adam_Nassar
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Drives the main project.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SpreadsheetForm());
+        }
+    }
+}
---
+++ CptS-321_Spreadsheet_Application/Spreadsheet_Adam_Nassar/SpreadsheetForm.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="SpreadsheetForm.cs" company="Adam Nassar 11588762">
+// Copyright (c) Adam Nassar 11588762. All rights reserved.
+// </copyright>
+
+namespace Spreadsheet_Adam_Nassar
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Basic Form Class.
+    /// </summary>
+    public partial class SpreadsheetForm : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpreadsheetForm"/> class.
+        /// </summary>
+        public SpreadsheetForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SpreadsheetForm_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
