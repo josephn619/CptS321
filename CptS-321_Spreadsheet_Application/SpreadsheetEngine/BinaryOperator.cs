03c6ef7302d99b363eaf8976bed4859d913c87d3
(Adam) Joseph Nassar <45108299+josephn619@users.noreply.github.com>
2022-03-02 01:29:52 -0800

    Added BinaryOperator Constructor, getters and setters.

--- CptS-321_Spreadsheet_Application/SpreadsheetEngine/BinaryOperator.cs
+++ CptS-321_Spreadsheet_Application/SpreadsheetEngine/BinaryOperator.cs
@@ -15,5 +15,45 @@ namespace Cpts321
     /// </summary>
     public class BinaryOperator : Node
     {
+        private char op;
+        private Node left;
+        private Node right;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryOperator"/> class.
+        /// </summary>
+        /// <param name="newOperator">newOperator.</param>
+        public BinaryOperator(char newOperator)
+        {
+            this.op = newOperator;
+            this.left = null;
+            this.right = null;
+        }
+
+        /// <summary>
+        /// Gets or sets op.
+        /// </summary>
+        public char Op
+        {
+            get
+            {
+                return this.op;
+            }
+
+            set
+            {
+                this.op = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets left.
+        /// </summary>
+        public Node Left { get; set; }
+
+        /// <summary>
+        /// Gets or sets right.
+        /// </summary>
+        public Node Right { get; set; }
     }
 }
