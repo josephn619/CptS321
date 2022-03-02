0a3d73c7958e98ec59766dc2049cc8328dca4a50
(Adam) Joseph Nassar <45108299+josephn619@users.noreply.github.com>
2022-03-02 00:39:11 -0800

    Added Expression Tree Console App

---
+++ CptS-321_Spreadsheet_Application/ExpTreeConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExpTreeConsole
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Expression Tree console app.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main driver for console app.
+        /// </summary>
+        /// <param name="args">args.</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CptS-321_Spreadsheet_Application/SpreadsheetEngine/ExpressionTree.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="ExpressionTree.cs" company="Adam Nassar 11588762">
+// Copyright (c) Adam Nassar 11588762. All rights reserved.
+// </copyright>
+
+namespace Cpts321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Class for ExpressTree.
+    /// </summary>
+    public class ExpressionTree
+    {
+
+    }
+}
