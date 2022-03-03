bf9b855edf19c4b577bed6e5a6cf0dfe765e4057
(Adam) Joseph Nassar <45108299+josephn619@users.noreply.github.com>
2022-03-02 20:56:48 -0800

    Added seperate test case file

--- CptS-321_Spreadsheet_Application/Spreadsheet_Tests/ExpressionTreeTests.cs
+++ CptS-321_Spreadsheet_Application/Spreadsheet_Tests/ExpressionTreeTests.cs
@@ -4,6 +4,7 @@
 
 namespace Spreadsheet_Adam_Nassar.Tests
 {
+    using System.Globalization;
     using NUnit.Framework;
 
     /// <summary>
@@ -12,5 +13,36 @@ namespace Spreadsheet_Adam_Nassar.Tests
     [TestFixture]
     public class ExpressionTreeTests
     {
+        /// <summary>
+        /// Test Evaluate method w/ normal cases.
+        /// </summary>
+        /// <param name="expression">expression.</param>
+        /// <returns>Evaluated expression.</returns>
+        [TestCase("3+5", ExpectedResult = 8)]
+        public double TestEvaluateNormalCases(string expression)
+        {
+            Cpts321.ExpressionTree testTree = new Cpts321.ExpressionTree(expression);
+            return 0.0;
+        }
+
+        /// <summary>
+        /// Tests if operator is considered unsupported.
+        /// </summary>
+        /// <param name="expression">expression.</param>
+        public void TestUnsupportedOperatorExpression(string expression)
+        {
+            Assert.That(() => new Cpts321.ExpressionTree(expression), Throws.TypeOf<System.Exception>());
+        }
+
+        /// <summary>
+        /// Tests Infinity cases.
+        /// </summary>
+        [Test]
+        public void TestInfinity()
+        {
+            // string maxValue = double.MaxValue.ToString("F",CultureInfo.InvariantCulture);
+            // double result = new Cpts321.ExpressionTree($"{maxValue}+{maxValue}").Evaluate();
+            // Assert.True(double.IsInfinity(result));
+        }
     }
 }
